d379eb9e95b09a7feb698d727677abf8424da4f2
Your Name <you@example.com>
2023-06-21 21:25:46 -0400

    hemos añadido un identificador único "contactForm

--- js/javascript.js
+++ js/javascript.js
@@ -1,36 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var myButton = document.getElementById("myButton");
+  var submitButton = document.getElementById("submitButton");
   
-    myButton.addEventListener("click", function() {
-      // Datos a enviar
-      var data = {
-        nombre: "John Doe",
-        email: "johndoe@example.com",
-        mensaje: "Hola, este es un mensaje de prueba"
-      };
-  
-      // Realizar la solicitud AJAX
-      fetch("url_del_servidor", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-      .then(function(response) {
-        if (response.ok) {
-          alert("Datos enviados correctamente");
-        } else {
-          alert("Ha ocurrido un error al enviar los datos");
-        }
-      })
-      .catch(function(error) {
-        alert("Ha ocurrido un error en la solicitud AJAX: " + error.message);
-      });
+  submitButton.addEventListener("click", function(event) {
+    event.preventDefault(); // Evitar el envío predeterminado del formulario
+
+    // Obtener los valores del formulario
+    var nombre = document.getElementById("nombre").value;
+    var email = document.getElementById("email").value;
+    var mensaje = document.getElementById("mensaje").value;
+
+    // Crear objeto de datos
+    var data = {
+      nombre: nombre,
+      email: email,
+      mensaje: mensaje
+    };
+
+    // Realizar la solicitud AJAX
+    fetch("url_del_servidor", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+    .then(function(response) {
+      if (response.ok) {
+        alert("Datos enviados correctamente");
+        // Restablecer los campos del formulario después de enviarlos con éxito
+        document.getElementById("nombre").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("mensaje").value = "";
+      } else {
+        alert("Ha ocurrido un error al enviar los datos");
+      }
+    })
+    .catch(function(error) {
+      alert("Ha ocurrido un error en la solicitud AJAX: " + error.message);
     });
   });
-  window.addEventListener('DOMContentLoaded', function() {
-    var descripcion = document.getElementById('descripcion');
-    descripcion.style.opacity = '2'; // Cambia la opacidad a 1
-  });
-  
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  var descripcion = document.getElementById('descripcion');
+  descripcion.style.opacity = '2'; // Cambia la opacidad a 1
+});

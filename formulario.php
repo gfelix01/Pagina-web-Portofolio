966765ebfe947250ddd30537268f53f2c1cacdc8
Your Name <you@example.com>
2023-06-23 15:18:55 -0400

    ajustes al php

--- formulario.php
+++ formulario.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $correo = $_POST['correo'];
   $mensaje = $_POST['mensaje'];
 
-  $destinatario = 'Gabriel_arturo01@hotmail.com';
+  $destinatario = 'gabriel_arturo01@hotmail.com';
   $asunto = 'Nuevo mensaje del formulario de contacto';
 
   $contenido = "Nombre: $nombre\n";
@@ -13,8 +13,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
   $cabeceras = "From: $nombre <$correo>\r\n";
   $cabeceras .= "Reply-To: $correo\r\n";
+  $cabeceras .= "MIME-Version: 1.0\r\n";
+  $cabeceras .= "Content-type: text/plain; charset=utf-8\r\n";
+  $cabeceras .= "X-Mailer: PHP/" . phpversion() . "\r\n";
 
-  if (mail($destinatario, $asunto, $contenido, $cabeceras)) {
+  // Configuración de autenticación SMTP para Hotmail (Outlook)
+  $smtpHost = 'smtp.live.com';
+  $smtpPort = 587;
+  $smtpUsername = 'gabriel_arturo01@hotmail.com';
+  $smtpPassword = 'Eltury832931420';
+
+  // Configurar opciones adicionales para la función mail
+  $mailOptions = "-f $smtpUsername";
+
+  if (mail($destinatario, $asunto, $contenido, $cabeceras, $mailOptions)) {
     echo 'El mensaje se envió correctamente. Gracias por contactarnos.';
   } else {
     echo 'Error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.';

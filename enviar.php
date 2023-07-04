ffaf801a1f40a82aa08274c35d96441c27a2531e
Your Name <you@example.com>
2023-07-03 21:01:03 -0400

    se agrego biblioteca PHP mailer

--- enviar.php
+++ enviar.php
@@ -1,4 +1,10 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php'; // Ruta al archivo autoload de PHPMailer
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $nombre = $_POST['nombre'];
   $correo = $_POST['correo'];
@@ -11,15 +17,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $contenido .= "Correo electrónico: $correo\n";
   $contenido .= "Mensaje: $mensaje\n";
 
-  $headers = "From: $nombre <$correo>";
+  $mail = new PHPMailer(true);
 
-  ini_set('SMTP', 'smtp.office365.com');
-  ini_set('smtp_port', 587);
+  try {
+    // Configuración del servidor SMTP
+    $mail->isSMTP();
+    $mail->Host = 'smtp.office365.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'gabriel_arturo01"hotmail.com'; // Reemplaza con tu dirección de correo electrónico
+    $mail->Password = 'Eltury832931420'; // Reemplaza con tu contraseña de correo electrónico
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
 
+    // Configuración del mensaje
+    $mail->setFrom($correo, $nombre);
+    $mail->addAddress($destinatario);
+    $mail->Subject = $asunto;
+    $mail->Body = $contenido;
 
-  if (mail($destinatario, $asunto, $contenido, $headers)) {
+    $mail->send();
+
     echo "¡Gracias por tu mensaje! Pronto te contactaré.";
-  } else {
+  } catch (Exception $e) {
     echo "Hubo un error al enviar el mensaje. Por favor, inténtalo nuevamente.";
   }
 }
